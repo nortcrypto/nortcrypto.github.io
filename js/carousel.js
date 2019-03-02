86b4f379a407eec98ef0949793968edf90790db0
kido72 <yprovorov@narod.ru>
2019-03-02 18:51:12 +0300

    add some changers 2

--- js/carousel.js
+++ js/carousel.js
@@ -1,4 +1,10 @@
-$('.carousel.carousel-slider').carousel({fullWidth: true,indicators: true});
+$('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+});
 var autoplay = true;
-setInterval(function() { if(autoplay) $('.carousel.carousel-slider').carousel('next'); }, 4500);
-$('.carousel.carousel-slider').hover(function(){ autoplay = false; },function(){ autoplay = true; });
+setInterval(function() { 
+if(autoplay)
+$('.carousel.carousel-slider').carousel('next'); }, 4500);
+$('.carousel.carousel-slider').hover(function(){ autoplay = false; },function(){ autoplay = true;
+});
