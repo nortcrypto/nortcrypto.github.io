ae56c87851d8285eab59a59a4328a3c5a7a3c6bb
kido72 <yprovorov@narod.ru>
2019-03-10 00:45:19 +0300

    menu transparent

--- js/nav.js
+++ js/nav.js
@@ -1,5 +1,14 @@
-$(document).ready(function() {
-  // plugin initializations etc.
-  $(".button-collapse").sideNav();
+//$(document).ready(function() {
+//  $(".button-collapse").sideNav();
+//});
 
+$(document).ready(function() {
+        $(window).scroll(function() {
+          if($(this).scrollTop() > 64) { 
+              $('.nav-bar').addClass('solid');
+          } else {
+              $('.nav-bar').removeClass('solid');
+          }
+        });
 });
+
