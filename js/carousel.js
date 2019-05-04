48d2dbde3c044c529bf0de767e6fdea9afe42b1b
kido72 <yprovorov@narod.ru>
2019-05-04 22:19:58 +0300

    some fix

--- js/carousel.js
+++ js/carousel.js
@@ -5,6 +5,6 @@ $('.carousel.carousel-slider').carousel({
 var autoplay = true;
 setInterval(function() { 
 if(autoplay)
-$('.carousel.carousel-slider').carousel('next'); }, 4500);
+$('.carousel.carousel-slider').carousel('next'); }, 8500);
 $('.carousel.carousel-slider').hover(function(){ autoplay = false; },function(){ autoplay = true;
 });
